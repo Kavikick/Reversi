dd0d81ea4b8c68fe86c7a15381e86c612207ace4
Wesley Duerksen <wesleypacs@gmail.com>
2019-12-08 18:40:18 +0000

    AutoPush

--- Project.java
+++ Project.java
@@ -158,7 +158,7 @@ class Side {
     }
 
     public boolean compare(Side friend) {
-        return face == friend.face();
+        return face == friend.getFace();
     }
 
     public Side clone() {
--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add -A
-git commit -m""
+git commit -m "AutoPush"
 git push
